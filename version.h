eeb5628257d12aa6e23fcb11596d03ca95ef9aaa
cloudzhou <cloudzhou@163.com>
2012-08-19 02:18:41 +0800

    change version, start as Openss

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 /* $OpenBSD: version.h,v 1.64 2012/02/09 20:00:18 markus Exp $ */
 
-#define SSH_VERSION	"Gitshell_OpenSSH_6.0"
+#define SSH_VERSION	"OpenSSH_Gitshell_6.0"
 
 #define SSH_PORTABLE	"p1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
