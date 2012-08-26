f9ded0873657010129315c74096c0977033be4ae
cloudzhou <cloudzhou@163.com>
2012-08-26 12:59:30 +0800

    add authorized_keys pri, *do remeber*

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@
 rm -rf /opt/run/openssh/
 make && make install
 sudo /opt/run/openssh/sbin/sshd
+
+## note ##
+#root@gitshell:/tmp/gitshellopenssh# ls -ld /home/git/.ssh/authorized_keys
+#-rw-r--r-- 1 git git 0 Aug 26 12:50 /home/git/.ssh/authorized_keys
+#root@gitshell:/tmp/gitshellopenssh# ls -ld /home/git/.ssh/
+#drwx------ 2 git git 4096 Aug 26 12:50 /home/git/.ssh/
